384942d3614339dcb30fc88058d735c9aed80cd1
vadivai <vaikanvn@gmail.com>
2024-02-20 17:33:23 +0200

    Add try/catch

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -10,59 +10,78 @@
 const httpError = require("../helpers/HttpError.js");
 const contactsService = require("../services/contactsServices.js");
 
-const getAllContacts = async (req, res) => {
-  const result = await contactsService.listContacts();
-  res.status(200).json(result);
-};
-
-const getOneContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await contactsService.getContactById(id);
-  if (!result) {
-    throw httpError(404);
+const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await contactsService.listContacts();
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json(result);
 };
 
-const deleteContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await contactsService.removeContact(id);
-  if (!result) {
-    throw httpError(404);
+const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.getContactById(id);
+    if (!result) {
+      throw httpError(404);
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json(result);
 };
 
-const createContact = async (req, res) => {
-  const result = await contactsService.addContact(req.body);
-  res.status(201).json(result);
-  // Отримує body в json-форматі з полями {name, email, phone}.
-  // Усі поля є обов'язковими - для валідації створи у файлі contactsSchemas.js (знаходиться у папці schemas) схему з використаням пакета joi
-  // Якщо в body немає якихось обов'язкових полів (або передані поля мають не валідне значення), повертає json формату {"message": error.message} (де error.message - змістовне повідомлення з суттю помилки) зі статусом 400
-  // Якщо body валідне, викликає функцію-сервіс addContact для роботи з json-файлом contacts.json, з передачею їй даних з body
-  // За результатом роботи функції повертає новостворений об'єкт з полями {id, name, email, phone} і статусом 201
-};
-
-const updateContact = async (req, res) => {
-  // Отримує body в json-форматі з будь-яким набором оновлених полів (name, email, phone) (всі поля вимагати в боді як обов'язкові не потрібно: якщо якесь із полів не передане, воно має зберегтись у контакта зі значенням, яке було до оновлення)
-  // Якщо запит на оновлення здійснено без передачі в body хоча б одного поля, повертає json формату {"message": "Body must have at least one field"} зі статусом 400.
-  // Передані в боді поля мають бути провалідовані - для валідації створи у файлі contactsSchemas.js (знаходиться у папці schemas) схему з використанням пакета joi. Якщо передані поля мають не валідне значення, повертає json формату {"message": error.message} (де error.message - змістовне повідомлення з суттю помилки) зі статусом 400
-  // Якщо з body все добре, викликає функцію-сервіс updateContact, яку слід створити в файлі contactsServices.js (знаходиться в папці services). Ця функція має приймати id контакта, що підлягає оновленню, та дані з body, і оновити контакт у json-файлі contacts.json
-  // За результатом роботи функції повертає оновлений об'єкт контакту зі статусом 200.
-
-  const { id } = req.params;
-  const { name, email, phone } = req.body;
-  const result = await contactsService.updateContact({
-    id,
-    name,
-    email,
-    phone,
-  });
-  if (!result) {
-    throw httpError(404);
-    //повертає json формату {"message": "Not found"} зі статусом 404
+const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
+    if (!result) {
+      throw httpError(404);
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json(result);
+};
+
+const createContact = async (req, res, next) => {
+  try {
+    const result = await contactsService.addContact(req.body);
+    res.status(201).json(result);
+    // Отримує body в json-форматі з полями {name, email, phone}.
+    // Усі поля є обов'язковими - для валідації створи у файлі contactsSchemas.js (знаходиться у папці schemas) схему з використаням пакета joi
+    // Якщо в body немає якихось обов'язкових полів (або передані поля мають не валідне значення), повертає json формату {"message": error.message} (де error.message - змістовне повідомлення з суттю помилки) зі статусом 400
+    // Якщо body валідне, викликає функцію-сервіс addContact для роботи з json-файлом contacts.json, з передачею їй даних з body
+    // За результатом роботи функції повертає новостворений об'єкт з полями {id, name, email, phone} і статусом 201
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  try {
+    // Отримує body в json-форматі з будь-яким набором оновлених полів (name, email, phone) (всі поля вимагати в боді як обов'язкові не потрібно: якщо якесь із полів не передане, воно має зберегтись у контакта зі значенням, яке було до оновлення)
+    // Якщо запит на оновлення здійснено без передачі в body хоча б одного поля, повертає json формату {"message": "Body must have at least one field"} зі статусом 400.
+    // Передані в боді поля мають бути провалідовані - для валідації створи у файлі contactsSchemas.js (знаходиться у папці schemas) схему з використанням пакета joi. Якщо передані поля мають не валідне значення, повертає json формату {"message": error.message} (де error.message - змістовне повідомлення з суттю помилки) зі статусом 400
+    // Якщо з body все добре, викликає функцію-сервіс updateContact, яку слід створити в файлі contactsServices.js (знаходиться в папці services). Ця функція має приймати id контакта, що підлягає оновленню, та дані з body, і оновити контакт у json-файлі contacts.json
+    // За результатом роботи функції повертає оновлений об'єкт контакту зі статусом 200.
+
+    const { id } = req.params;
+    const { name, email, phone } = req.body;
+    const result = await contactsService.updateContact({
+      id,
+      name,
+      email,
+      phone,
+    });
+    if (!result) {
+      throw httpError(404);
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -34,7 +34,7 @@ async function addContact({ name, email, phone }) {
 
 async function updateContact({ id, name, email, phone }) {
   const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === contactId);
+  const index = contacts.findIndex((item) => item.id === id);
   if (index === -1) return null;
   contacts[index] = { id, name, email, phone };
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
