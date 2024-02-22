87474ca4048e29cd38cc7a2b7aa66b1738a59edf
vadivai <vaikanvn@gmail.com>
2024-02-22 20:50:12 +0200

    Commit 1

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+// require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const { DB_HOST, PORT = 3000 } = process.env;
+// const PORT = 3000;
+// const DB_HOST =
+//   "mongodb+srv://admin:7X855Z5sRvbwcpG9@cluster0.ibb15yf.mongodb.net/Library?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connect success");
+    // app.listen(PORT);
+    app.listen(PORT, () => {
+      console.log("Server is running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 const contactsRouter = require("./routes/contactsRouter");
 
@@ -22,6 +48,6 @@ app.use((err, req, res, next) => {
   // console.log(status, message);
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server is running. Use our API on port: 3000");
+// });
